5b486bc33c02ee33c1b69d866b0c431ec5d264e5
Naoki Kaneko <lpeopleocomevtogethere@gmail.com>
2023-03-02 02:09:14 +0900

    snapshot撮り直し

--- src/storybook.test.js
+++ src/storybook.test.js
@@ -1,2 +1,3 @@
 import initStoryshots from '@storybook/addon-storyshots';
-initStoryshots();
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+initStoryshots({ suite: 'Image storyshots', test: imageSnapshot() });
